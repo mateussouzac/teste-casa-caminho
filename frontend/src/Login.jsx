9653e6b8d1eac4e527fb91d2c49e64a6b59119a1
mateussouzac <mateus_souzacampos@outlook.com>
2025-11-30 20:34:54 -0300

    Correção bug login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -73,7 +73,7 @@ export default function Login() {
                 value={email} 
                 onChange={(e) => setEmail(e.target.value)} 
                 required 
-                placeholder="ex: admin@casacaminho.org"
+                placeholder="Digite seu e-mail"
               />
             </div>
 
