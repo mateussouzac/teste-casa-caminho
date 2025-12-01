44ba64390aa610731ad917959685470259520c55
Paula Bessa <1557309@sga.pucminas.br>
2025-12-01 10:09:57 -0300

    alteração lista de espera

--- frontend/src/ListaDeEspera.jsx
+++ frontend/src/ListaDeEspera.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './ListaDeEspera.css';
 import logoImg from './assets/logo.png'; // Reutilizando a logo que você já tem
 
